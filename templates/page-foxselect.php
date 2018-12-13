ff409218f6e9d99a586d6426fbe479101e8cb4d4
Michael Wender <michael@michaelwender.com>
2018-12-13 11:26:46 -0500

    Adding FOXSelect React app

--- templates/page-foxselect.php
+++ templates/page-foxselect.php
@@ -9,12 +9,39 @@ namespace Fox\foxselect;
 if ( ! defined( 'WPINC' ) ) {
     die;
 }
-/*
-function enqueue_scripts(){
 
+/**
+ * Removes default theme styles and loads FOXSelect React app
+ */
+function enqueue_scripts(){
+  wp_dequeue_style( 'business-pro-min' );
+  wp_enqueue_style( 'foxselect-theme', get_stylesheet_directory_uri() . '/assets/css/foxselect.css', [], filemtime( get_stylesheet_directory() . '/assets/css/foxselect.css') );
+
+  /**
+   * Load FOXSelect React app JS
+   */
+  $scripts = glob( get_stylesheet_directory() . '/assets/scripts/foxselect/static/js/main.*' );
+  foreach ($scripts as $script) {
+    if( '.js' == substr( $script, -3 ) ){
+      wp_enqueue_script( 'foxselect', get_stylesheet_directory_uri() . '/assets/scripts/foxselect/static/js/' . basename( $script ), null, null, true );
+      wp_localize_script( 'foxselect', 'wpvars', [
+        'root' => esc_url_raw( rest_url() ),
+        'nonce' => wp_create_nonce( 'wp_rest' ),
+        'authroute' => 'jwt-auth/v1/token',
+      ]);
+    }
+  }
+
+  /**
+   * Load FOXSelect React app CSS
+   */
+  $styles = glob( get_stylesheet_directory() . '/assets/scripts/foxselect/static/css/main.*' );
+  foreach ($styles as $style) {
+    if( '.css' == substr( $style, -4 ) )
+      wp_enqueue_style( 'foxselect', get_stylesheet_directory_uri() . '/assets/scripts/foxselect/static/css/' . basename( $style ) );
+  }
 }
-*/
-//\add_action( 'wp_enqueue_scripts', $function_to_add, $priority = 10, $accepted_args = 1 )
+\add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts', 100 );
 
 /**
  * Add full-width page body class to the head.
@@ -56,16 +83,28 @@ function genesis_after_header(){
 \add_action( 'genesis_after_header', __NAMESPACE__ . '\\genesis_after_header', 11 );
 
 
+function genesis_entry_content(){
+    ?>
+    <noscript>
+      You need to enable JavaScript to run this app.
+    </noscript>
+    <div class="foxselect twbs" id="root"></div>
+    <?php
+}
+\add_action( 'genesis_entry_content', __NAMESPACE__ . '\\genesis_entry_content', 5 );
+
+/**
+ * Removes footer CTA row
+ */
 function genesis_footer(){
     $genesis_footer = [
         'business_before_footer_widget_area' => 5,
-        //'genesis_footer_widget_areas' => 6,
     ];
     foreach ($genesis_footer as $function => $priority) {
         remove_action( 'genesis_footer', $function, $priority );
     }
 }
-//\add_action( 'genesis_after_content', __NAMESPACE__ . '\\genesis_footer', 10 );
+\add_action( 'genesis_after_content', __NAMESPACE__ . '\\genesis_footer', 10 );
 
 // Run the Genesis loop.
 \genesis();
